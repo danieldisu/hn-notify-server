c833572a4bf7f767b854211f050de2d676ccb875
danieldisu <danieldiazsuarez@gmail.com>
2019-01-30 18:29:10 +0100

    First commit

---
+++ src/main/kotlin/com/danieldisu/hnnotify/HNNotifyApplication.kt
@@ -0,0 +1,12 @@
+package com.danieldisu.hnnotify
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HNNotifyApplication
+
+fun main(args: Array<String>) {
+	runApplication<HNNotifyApplication>(*args)
+}
+
