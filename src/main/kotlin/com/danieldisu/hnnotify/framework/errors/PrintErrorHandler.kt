379c01ffc9c3628262b761b792d061e7eda6ccab
danieldisu <danieldiazsuarez@gmail.com>
2019-02-02 10:54:39 +0100

    change handle error impl

--- src/main/kotlin/com/danieldisu/hnnotify/framework/errors/PrintErrorHandler.kt
+++ src/main/kotlin/com/danieldisu/hnnotify/framework/errors/PrintErrorHandler.kt
@@ -1,12 +1,15 @@
 package com.danieldisu.hnnotify.framework.errors
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class PrintErrorHandler : ErrorHandler {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun handle(throwable: Throwable) {
-        throwable.printStackTrace()
+        logger.error("handledError", throwable)
     }
 
 }
