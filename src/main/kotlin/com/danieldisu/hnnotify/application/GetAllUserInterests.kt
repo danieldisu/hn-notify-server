7bd81c63cb69f089150a142a9eafe615d9a6e553
danieldisu <danieldiazsuarez@gmail.com>
2019-02-03 16:41:51 +0100

    Implemented GET /interests endpoint

---
+++ src/main/kotlin/com/danieldisu/hnnotify/application/GetAllUserInterests.kt
@@ -0,0 +1,17 @@
+package com.danieldisu.hnnotify.application
+
+import com.danieldisu.hnnotify.domain.data.Interest
+import com.danieldisu.hnnotify.framework.repositories.interest.InterestRepository
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+@Service
+class GetAllUserInterests(private val interestRepository: InterestRepository) {
+
+    fun execute(userId: String): Mono<List<Interest>> {
+        return Mono.fromCallable {
+            interestRepository.findAllByUserId(userId)
+                .map { interestDBO -> Interest(interestDBO.interestName, userId) }
+        }
+    }
+}
--- src/main/kotlin/com/danieldisu/hnnotify/framework/controllers/InterestsController.kt
+++ src/main/kotlin/com/danieldisu/hnnotify/framework/controllers/InterestsController.kt
@@ -1,6 +1,7 @@
 package com.danieldisu.hnnotify.framework.controllers
 
 import com.danieldisu.hnnotify.application.AddNewInterestForUser
+import com.danieldisu.hnnotify.application.GetAllUserInterests
 import com.danieldisu.hnnotify.domain.data.Interest
 import com.danieldisu.hnnotify.framework.controllers.data.InterestDTO
 import org.springframework.http.MediaType
@@ -11,7 +12,10 @@ import java.net.URI
 
 @RestController
 @RequestMapping("/user/{userId}/interests")
-class InterestsController(private val addNewInterestForUser: AddNewInterestForUser) {
+class InterestsController(
+    private val addNewInterestForUser: AddNewInterestForUser,
+    private val getAllUserInterests: GetAllUserInterests
+) {
 
     @PostMapping("", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun addInterest(
@@ -20,6 +24,12 @@ class InterestsController(private val addNewInterestForUser: AddNewInterestForUs
     ): Mono<ResponseEntity<Unit>> {
         return addNewInterestForUser.execute(Interest(interest.interestName, userId))
             .map { ResponseEntity.created(URI("/user/$userId/interests")).build<Unit>() }
+    }
+
+    @GetMapping("", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getAllInterests(@PathVariable userId: String): Mono<List<InterestDTO>> {
+        return getAllUserInterests.execute(userId)
+            .map { interests -> interests.map { InterestDTO(it.interestName) } }
     }
 
 }
