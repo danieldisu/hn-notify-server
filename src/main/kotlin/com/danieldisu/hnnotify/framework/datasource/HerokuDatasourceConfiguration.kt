fc0e3791cf36f1803dfae5f0401ced4472660ff6
danieldisu <danieldiazsuarez@gmail.com>
2021-05-16 09:42:19 +0200

    Build db configuration programmatically

---
+++ src/main/kotlin/com/danieldisu/hnnotify/framework/datasource/HerokuDatasourceConfiguration.kt
@@ -0,0 +1,31 @@
+package com.danieldisu.hnnotify.framework.datasource
+
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import java.net.URI
+import javax.sql.DataSource
+
+
+@Configuration
+@Profile("prod")
+class HerokuDatasourceConfiguration {
+
+    @Bean
+    fun getHerokuDatasource(): DataSource {
+        val dbUri = URI(System.getenv("DATABASE_URL"))
+
+        val username = dbUri.userInfo.split(":").toTypedArray()[0]
+        val password = dbUri.userInfo.split(":").toTypedArray()[1]
+        val jdbcCompatibleDBUrl =
+                "jdbc:postgresql://" + dbUri.host + ':' + dbUri.port + dbUri.path + "?sslmode=require"
+
+        return DataSourceBuilder.create()
+                .url(jdbcCompatibleDBUrl)
+                .username(username)
+                .password(password)
+                .build()
+    }
+
+}
--- src/main/kotlin/com/danieldisu/hnnotify/framework/jobs/FetchLatestNewStoriesJob.kt
+++ src/main/kotlin/com/danieldisu/hnnotify/framework/jobs/FetchLatestNewStoriesJob.kt
@@ -5,9 +5,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
-const val tenMinutes = 600000L
-const val twentySeconds = 20000L
-
 @Component
 class FetchLatestNewStoriesJob(private val fetchLatestNewStories: FetchLatestNewStories) {
 
